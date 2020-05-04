54891a92ff7960a2c7662fc5969bcecd9fdea710
shenpeihong <939247689@qq.com>
2020-05-05 00:52:29 +0800

    20200505

---
+++ src/main/java/com/shen/ssmschoolshop/util/FieldDef.java
@@ -0,0 +1,15 @@
+package com.shen.ssmschoolshop.util;
+
+/**
+ * Created by Shen Peihong on 2020/4/22 16:28
+ */
+
+public class FieldDef {
+    //超管
+    public static final int superManager = 1;
+
+    //普管
+    public static final int ordManager = 0;
+
+
+}
